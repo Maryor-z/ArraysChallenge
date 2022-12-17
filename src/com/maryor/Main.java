0c1c24628abb6cd9f7f01602dd8dcb4f6eed744c
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-17 09:12:50 +0100

    Update Main.java

--- src/com/maryor/Main.java
+++ src/com/maryor/Main.java
@@ -1,2 +1,59 @@
-package com.maryor;public class Main {
+package com.maryor;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        int[] myIntegers = getIntegers(6);
+        int[] sorted = sortIntegers(myIntegers);
+        printArray(sorted);
+
+    }
+
+    public static int[] getIntegers(int capacity){
+
+        int[] array = new int[capacity];
+        System.out.println("Enter " + capacity + " integer values");
+
+        for (int i = 0; i < array.length; i++){
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array){
+
+        for (int j=0; j< array.length; j++){
+            System.out.println("Element " + j + " contents " + array[j]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array){
+//        int[] sortedArray = new int[array.length];
+//        for (int i = 0; i < array.length; i++){
+//                sortedArray[i] = array[i];
+//        }
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+        boolean flag = true;
+        int temp;
+        while (flag) {
+            flag = false;
+//            element 0     160
+//            element 1     50
+//            element 2     40
+            for (int i=0; i<sortedArray.length-1; i++) {
+                if (sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+       return sortedArray;
+    }
 }
